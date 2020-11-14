a9c24cd9e003670bdb281ad35216c9d16bcfb4d0
Yuri Domingos <64822018+YuriDomingos@users.noreply.github.com>
2020-11-14 05:30:43 +0100

    Add files via upload
    Single commit

---
+++ README.md
@@ -0,0 +1,32 @@
+
+
+#PROLOG (NÍVEL BÁSICO)
+Data : 14 - 11 -2020 )
+@Autor : Yuri Domingos
+UCAN  : Universidade Católica de Angola
+
+
+
+         #Árvore Genealógica da Família Pinheiro
+         
+                           
+    Enunciado:
+    
+  Pouco se sabe da história passada da família Pinheiro. Existem alguns registos
+antigos que indicam que o casal José e Maria criou dois filhos, o João e a Ana. Que a
+Ana teve duas filhas, a Helena e a Joana, também parece ser verdade, segundo os
+mesmos registos. Além disso, o Mário é filho do João, pois muito se orgulha ele
+disso. Estranho também, foi constatar que o Carlos nasceu da relação entre a Helena,
+muito formosa, e o Mário.
+
+a) Utilizando o predicado progenitor(X,Y) (ou seja, X é progenitor de Y),
+represente em Prolog todos os progenitores da família Pinheiro.
+b) Represente em Prolog as relações: sexo (masculino ou feminino), irmã,
+irmão, descendente, mãe, pai, avô, tio, primo 2 .
+c) Formule em Prolog as seguintes questões:
+1. O João é filho do José?
+2. Quem são os filhos da Maria?
+3. Quem são os primos do Mário?
+4. Quantos sobrinhos/sobrinhas com um Tio existem na família Pinheiro?
+5. Quem são os ascendentes do Carlos?
+6. A Helena tem irmãos? E irmãs?
---
+++ arvoreGeneologica.pl
@@ -0,0 +1,108 @@
+/*
+Autor : Yuri Domingos 
+Data  : 13 - 11 - 2020
+Objectivo : Resolver o clássico problema da arvore geneologica
+Linguagem  : Prolog
+*/
+
+
+/*
+Problema : 
+O clássico  problema da familia pinheiro
+*/
+
+/* PROGENITORES EXISTENTES DA FAMÍLIA PINHEIRO */
+
+
+
+
+
+
+
+progenitor(jose,joao).
+progenitor(jose,ana).
+progenitor(maria,joao).
+progenitor(maria,ana).
+progenitor(ana,helena).
+progenitor(ana,joana).
+progenitor(joao,mario).
+progenitor(helena,carlos).
+progenitor(mario,carlos).
+progenitor(carlos,yuri).
+
+
+/*------------ SEXO DAS PESSOAS  EXISTENTE NA FAMILIA PINHEIRO----*/
+
+homem(jose).
+homem(joao).
+homem(carlos).
+homem(mario).
+homem(yuri). 
+
+
+mulher(ana).
+mulher(maria).
+mulher(helena).
+mulher(joana).
+
+
+
+sexo(jose,masculino).
+sexo(joao,masculino).
+sexo(carlos,masculino).
+sexo(mario,masculino).
+sexo(yuri,masculino).
+
+sexo(ana,feminino).
+sexo(maria,feminino).
+sexo(helena,feminino).
+sexo(joana,feminino).
+
+
+
+/* ------------- CONSTRUCAO DE REGRAS  ---------------------*/
+
+pai(X,Y) :- sexo(X,masculino), progenitor(X,Y).
+
+mae(X,Y) :- sexo(X,feminino), progenitor(X,Y).
+
+
+/* ------------ A regra filho não foi solicitada, mas para facilitar a lógica do velho programdor, foi implementada */
+
+filho(X,Y) :- sexo(X,masculino) , progenitor(Y,X).
+
+filha(X,Y) :- sexo(X,feminino), progenitor(Y,X).
+
+/*------------------------------------------------------------------------------------- */
+
+
+irmao(X,Y) :- sexo(X,masculino), progenitor(Z,Y), progenitor(Z,X) , X \== Y.
+
+irma(X,Y) :- sexo(X,feminino), progenitor(Z,Y),progenitor(Z,X), X \== Y.
+
+primo(X,Y) :- sexo(X,masculino), progenitor(Z,X), irmao(Z,P), progenitor(P,Y).
+
+prima(X,Y) :- sexo(X, feminino), progenitor(Z,X), irmao(Z,P), progenitor(P,Y).
+
+tio(X,Y)   :- sexo(X,masculino) ,  progenitor(P,Y),  irmao(P,X). 
+
+tia(X,Y)   :- sexo(X,feminino), progenitor(P,Y),  irma(P,X).
+
+
+avo(X,Y)   :- sexo(X,masculino), progenitor(X,P), progenitor(P,Y).
+
+bisavoHomem(X,Y) :- sexo(X,masculino), progenitor(X,P), avo(P,Y).
+
+/*--------------- Clássica regra do descendente ( pesquisa recursiva com caso baso    --------------------------------*/
+
+
+descendente(X,Y):- progenitor(X,Y).
+
+descendente(X,Y) :- progenitor(X,P) , descendente(P,Y).
+
+
+
+
+
+
+
